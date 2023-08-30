9fe7f8a2dbdfde109d941fb85f5bf3e4bdc5ba12
huynguyen2842 <gianghuy2804@gmail.com>
2023-08-30 23:01:47 +0700

    Initial Commit

---
+++ TaiXiuApp/TaiXiuAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaiXiuAppApp.swift
+//  TaiXiuApp
+//
+//  Created by Nguyen Giang Huy on 30/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct TaiXiuAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
